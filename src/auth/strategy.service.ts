9a837ac501ce9eb6fe1ad97ce311c63a1f72bada
wetterene <wetterene.remy@gmail.com>
2023-06-20 16:51:47 +0200

    test ajout cookie

--- src/auth/strategy.service.ts
+++ src/auth/strategy.service.ts
@@ -5,6 +5,7 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { ExtractJwt, Strategy } from 'passport-jwt';
 import { UserEntity } from 'src/shared/entities/user.entity';
 import { Repository } from 'typeorm';
+import { Request as RequestType } from 'express';
 
 type Payload = {
   sub: number;
@@ -17,10 +18,20 @@ export class strategyService extends PassportStrategy(Strategy) {
     @InjectRepository(UserEntity) readonly userRepo: Repository<UserEntity>,
   ) {
     super({
-      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      jwtFromRequest: ExtractJwt.fromExtractors([
+        strategyService.extractJWT,
+        ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ]),
       secretOrKey: config.get('SECRET_TOKEN_KEY'),
       ignoreExpiration: false,
     });
+  }
+
+  private static extractJWT(req: RequestType): string | null {
+    if (req.cookies && 'token' in req.cookies && req.cookies.token.length > 0) {
+      return req.cookies.token;
+    }
+    return null;
   }
 
   async validate(payload: Payload) {
--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,13 @@ import { ClassSerializerInterceptor } from '@nestjs/common/serializer';
 import { GlobalExceptionFilter } from './shared/handler/error.handler';
 import { ConfigService } from '@nestjs/config';
 import * as session from 'express-session';
+import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const config = app.get(ConfigService);
+  app.use(cookieParser());
   app.enableCors();
   const PORT = +config.get<number>('PORT');
   app.use(
@@ -16,10 +18,15 @@ async function bootstrap() {
       secret: config.get('SECRET_TOKEN_KEY'),
       resave: false,
       saveUninitialized: false,
+      cookie: {
+        httpOnly: true,
+        maxAge: 500,
+        signed: config.get('SECRET_TOKEN_KEY'),
+      },
     }),
   );
   app.setGlobalPrefix('api');
-  // app.useGlobalFilters(new GlobalExceptionFilter());
+  app.useGlobalFilters(new GlobalExceptionFilter());
   // app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
 
